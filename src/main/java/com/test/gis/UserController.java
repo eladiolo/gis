15b1a2e63dd17059c1e8c96916de2c8d6290283d
Eladio <eladiodefresa@gmail.com>
2023-03-08 22:29:47 +0100

    Create UserController.java

---
+++ src/main/java/com/test/gis/UserController.java
@@ -0,0 +1,24 @@
+package com.test.gis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+	  // Aggregate root
+	  // tag::get-aggregate-root[]
+	  @GetMapping("/employees")
+	  List<String> all() {
+		  List<String> strings = new ArrayList<>();
+		  strings.add("Hola");
+		  strings.add("Adios");
+		  
+		  return strings;
+	  }
+}
+
+
